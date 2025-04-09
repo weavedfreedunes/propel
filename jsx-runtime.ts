b03b6d05dca43aaba555aba9ab78abf92d0c2dd5
weavedfreedunes <weavedfreedunes@icloud.com>
2025-04-09 10:51:59 -0700

    minor: jsx escape

--- jsx-runtime.ts
+++ jsx-runtime.ts
@@ -4,3 +4,38 @@ export const jsxAttr = (
 ): string => {
 	return `${name}="${value}"`;
 };
+
+export const jsxEscape = (
+	value: boolean | number | object | string,
+): string => {
+	const string = `${value}`;
+	if (string.length === 0 || /["&<]/.test(string) === false) {
+		return string;
+	}
+
+	const encode = (
+		remaining: string,
+		index: number,
+		string2: string,
+	): string => {
+		if (index >= remaining.length) {
+			return string2;
+		}
+
+		const entity = {
+			34: "&quot;",
+			38: "&amp;",
+			39: "&apos;",
+			60: "&lt;",
+			62: "&gt;",
+		}[remaining.charCodeAt(index)];
+
+		if (!entity) {
+			return encode(remaining, index + 1, string2 + remaining.charAt(index));
+		}
+
+		return encode(remaining, index + 1, string2 + entity);
+	};
+
+	return encode(string, 0, "");
+};
